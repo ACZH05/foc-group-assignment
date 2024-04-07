729f2e2fab4968f8313f59f3805405d489992d5c
Alfred Chin <alfredchin2005@gmail.com>
2024-04-07 14:25:03 +0800

    Create interface for ICT course

--- index.cpp
+++ index.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class courseInfo
@@ -15,22 +16,31 @@ public:
     }
 };
 
+void createTable(courseInfo courseList[]);
+
 int main()
 {
-    // create an object for autoCAD Basic
-    courseInfo autoCAD("1-AutoCAD Basic", 1, 1470);
+    courseInfo autoCAD("1-AutoCAD Basic", 1, 1470);                    // create an object for autoCAD Basic
+    courseInfo msExcelBasic("2-Microsoft Excel Basic", 2, 525);        // create an object for Microsoft Excel Basic
+    courseInfo msProject("3-Microsoft Project", 3, 1570);              // create an object for Microsoft Project
+    courseInfo eInvoice("4-E-Invoice Strategic Implementing", 4, 850); // create an object for Invoice Strategic Implementing
+    courseInfo analyzingData("5-Analyzing Data", 5, 1890);             // create an object for Analyzing Data
 
-    // create an object for Microsoft Excel Basic
-    courseInfo msExcelBasic("2-Microsoft Excel Basic", 2, 525);
+    courseInfo courseList[5] = {autoCAD, msExcelBasic, msProject, eInvoice, analyzingData};
 
-    // create an object for Microsoft Project
-    courseInfo msProject("3-Microsoft Project", 3, 1570);
-
-    // create an object for Invoice Strategic Implementing
-    courseInfo eInvoice("4-E-Invoice Strategic Implementing", 4, 850);
-
-    // create an object for Analyzing Data
-    courseInfo analyzingData("5-Analyzing Data", 5, 1890);
-
+    createTable(courseList);
     return 0;
+}
+
+void createTable(courseInfo courseList[])
+{
+    cout << "| " << setw(40) << left << "Course"
+         << "| " << setw(20) << left << "Course code"
+         << "| Fee " << setw(18) << left << "(RM)"
+         << "|\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "| " << setw(40) << left << courseList[i].courseName << "| " << setw(20) << left << courseList[i].courseCode << "| RM" << setw(20) << left << courseList[i].courseFee
+             << "|\n";
+    }
 }
